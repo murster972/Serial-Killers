d77202ee147cf1c8d4231cba4a4dfd49c8ed2456
Murray Watson <murster972@yahoo.com>
2018-09-04 22:31:49 +0100

    init

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -6,7 +6,7 @@ var killerNames = [];
 var killersInfo = {};
 
 function getSerialKillerData(){
-    url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=List_of_serial_killers_by_number_of_victims&callback=?&section=";
+    url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=List_of_serial_killers_by_number_of_victims&callback=?&section=";
 
     // gets each section from wiki page https://en.wikipedia.org/wiki/List_of_serial_killers_by_number_of_victims#Serial_killers_with_the_highest_known_victim_count
     for(let section = 1; section <= numberOfSections; section++){
