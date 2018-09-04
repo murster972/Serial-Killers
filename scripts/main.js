b1667045d521661ad4e0a6431466e584d96e16af
Murray Watson <murster972@yahoo.com>
2018-09-04 22:22:49 +0100

    init

---
+++ scripts/main.js
@@ -0,0 +1,210 @@
+// NOTE: REMOVE TOKEN BEFORE PUTTING ON PUBLIC REPO!!!
+var apiToken = "pk.eyJ1IjoibXVyc3Rlcjk3MiIsImEiOiJjamxsNHA1MDcwcjNxM3BuZGkxb2pzYmxvIn0.ZQkVI5WPeqK4hLjtFrCBIQ";
+
+var Worldmap, geojson
+
+var countryNames = [];
+var countryCodes = {};
+
+// serial killer data
+getSerialKillerData();
+var countries = {};
+var maxVictims = 0;
+
+$(document).ready(function(){
+    $(".countryInfo").hover(function(){
+        console.log(this)
+        // $(this).toggleClass("show");
+    })
+})
+
+// required to use functions in p5.js
+function setup(){}
+function draw(){}
+
+window.onload = function(){
+    getCountryNames();
+    countryKillers();
+    createMap();
+}
+
+function createMap(){
+    mapboxgl.accessToken = apiToken;
+
+    var Worldmap = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/murster972/cjlnle0093vz82rmhw4f3e5gh',
+        // style: "mapbox://styles/murster972/cjlnlg0cd6c332ro3vjk5v5t8",
+        // style: "mapbox://styles/murster972/cjlnljjlf46lo2spj27s4z041",
+        // style: "mapbox://styles/murster972/cjlnljnrb46cz2rmtgd787knq",
+        center: [-65.017, -16.457],
+        pitch: 30,
+        zoom: 1,
+        maxZoom: 5,
+        minZoom: 1,
+        attributionControl: false,
+        // stops user being able to scroll map when at edge, also stops map from going of screen
+        // and having white bar appear at top of screen
+        maxBounds: [
+            [-180, -90],
+            [180, 80]
+        ]
+    });
+
+
+    Worldmap.on("load", function(){
+        // remove all default labels from map
+        Worldmap.style.stylesheet.layers.forEach(function(layer) {
+            if (layer.type === 'symbol') {
+                Worldmap.removeLayer(layer.id);
+            }
+        });
+
+        // polgon test
+        // console.log(countryPolygons);
+        for(let ind = 0; ind < 255; ind++){
+            let cInfo = countryPolygons["features"][ind];
+            let cName = cInfo["properties"]["ADMIN"];
+            let polygon = cInfo["geometry"]["coordinates"];
+            let type = cInfo["geometry"]["type"];
+
+            if(!(cName in countries) || countries[cName]["victim count"] == 0) continue;
+
+            // maps victim count to red value
+            // from 10 and not 0 to prevent black bg on countries with killers
+            let red_val = map(countries[cName]["victim count"], 0, maxVictims, 10, 255);
+
+            Worldmap.addLayer({
+                'id': cName,
+                'type': 'fill',
+                'source': {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': type,
+                            'coordinates': polygon
+                        }
+                    }
+                },
+                'paint': {
+                    // rgb(184,49,47)
+                    'fill-color': 'rgb('+ red_val.toString() +', 0, 0)',
+                    'fill-opacity': 1
+                }
+            });
+        }
+    })
+
+    // https://www.mapbox.com/mapbox-gl-js/example/custom-marker-icons/
+    // adding customer markers, see link above for details
+    geojson = {"type": "FeatureCollection", "features" :[]};
+
+    for(let c of countryCentroids){
+        let name = c["country"];
+        let lat = parseInt(c["lat"]);
+        let lng = parseInt(c["lng"]);
+
+        // skip countries that have no killers or dont exsist in countries keys;
+        if(!(name in countries) || countries[name]["victim count"] == 0) continue;
+
+        geojson["features"].push({
+           "countryName": name,
+           "countryCode": countryCodes[name],
+           "killers": countries[name]["killer names"],
+           "victim count": countries[name]["victim count"],
+           "killer count": countries[name]["killer count"],
+           "type": "Feature",
+           "properties": {
+               "description": countries[name]["killer names"],
+               "iconSize": [20, 20]
+           },
+           "geometry": {
+               "type": "Point",
+               "coordinates": [
+                   lng,
+                   lat
+               ]
+           }
+       })
+    }
+
+    // add customer markers to map
+    geojson.features.forEach(function(marker){
+        let info = "";
+
+        let flagUrl = "images/flags/" + marker["countryCode"].toLowerCase()  + ".svg";
+
+
+        for(let killer of marker["killers"]){
+            let i = killersInfo[killer]
+
+            let d = "<div class='killerInfo'>\
+                        <div class='img'></div>\
+                        <h2>" + killer + "</h2>\
+                        <h3>Proven Victims: " + i["Proven Victims"].toString() + "</h3>\
+                        <h3>Active: " + i["Years Active"]["start"].toString() + " to " + i["Years Active"]["end"].toString() + "</h3>\
+                        <p>" + i["notes"] + "</p>\
+                    </div>"
+            info += d;
+        }
+
+        // info for on click
+        let expandDiv = "<div class='popup'>\
+            <p>Victim Count: " + marker["victim count"] + "</p>\
+            <p>Number of Killers: " + marker["killer count"] + "</p>\
+            " + info + "\
+        </div>"
+
+        // create div container for marker
+        let div = document.createElement("div");
+        div.className = "countryInfo";
+        div.innerHTML = "<div class='countryTitle'>" + marker["countryName"] + "<img class='flag' src='" + flagUrl + "'></div>" + expandDiv
+
+        // add div to map
+        new mapboxgl.Marker(div).setLngLat(marker.geometry.coordinates).addTo(Worldmap);
+    })
+
+    // // pop ups on custom markers
+    // for(let c of countryNames){
+    //     map.on("click", c, function(e){
+    //         console.log(e);
+    //     })
+    // }
+}
+
+/* extracts country names from country_info.js */
+function getCountryNames(){
+    for(let i = 0; i < country_info["ref_country_codes"].length; i++){
+        let country = country_info["ref_country_codes"][i]["country"];
+        countryNames.push(country);
+        countryCodes[country] = country_info["ref_country_codes"][i]["alpha2"];
+    }
+}
+
+// sorts killers into countries and gets max victims
+function countryKillers(){
+    // init country object with country names as jeys
+    for(let i = 0; i < countryNames.length; i++){
+        let name = countryNames[i];
+        countries[name] = {"victim count": 0, "killer count": 0, "killer names": []};
+    }
+
+    for(let i = 0; i < killerNames.length; i++){
+        let name = killerNames[i]
+        let info = killersInfo[name];
+
+        for(let c of info["countries"]){
+            if(!(c in countries)) continue;
+            countries[c]["victim count"] += info["Proven Victims"];
+            countries[c]["killer count"] ++;
+            countries[c]["killer names"].push(name);
+
+        }
+    }
+
+    for(let c of countryNames){
+        let v = countries[c]["victim count"];
+        if(v > maxVictims) maxVictims = v;
+    }
+}
---
+++ scripts/wiki.js
@@ -0,0 +1,167 @@
+/* uses wiki api to fetch stats about serial killers */
+
+const numberOfSections = 4;
+
+var killerNames = [];
+var killersInfo = {};
+
+function getSerialKillerData(){
+    url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=List_of_serial_killers_by_number_of_victims&callback=?&section=";
+
+    // gets each section from wiki page https://en.wikipedia.org/wiki/List_of_serial_killers_by_number_of_victims#Serial_killers_with_the_highest_known_victim_count
+    for(let section = 1; section <= numberOfSections; section++){
+        let d = getWikiData(url + section);
+    }
+}
+
+function getImage(){
+
+    //BUG: the SHITE fucking code below is unable to send and recv a response to/from wiki api
+    //     to get the json data.
+    // $.ajax({
+    //     type: "GET",
+    //     url: "http://en.wikipedia.org/w/api.php?action=query&titles=Ted Bundy&prop=pageimages&format=json",
+    //     contentType: "application/json; charset=utf-8",
+    //     async: true,
+    //     dataType: "json",
+    //     success: function(d, textStatus, jqXHR){
+    //         console.log(d);
+    //     },
+    //     error: function(err){
+    //         console.log("error", err);
+    //     }
+    // })
+}
+
+function getWikiData(url){
+    $.ajax({
+        type: "GET",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function(d, textStatus, jqXHR){
+            let data = d.parse.text["*"];
+            parseWikiResults(data);
+            // return parsed;
+        },
+        error: function(err){
+            console.log("error", err);
+        }
+    })
+}
+
+/* get data from tables in data */
+function parseWikiResults(data){
+    let lines = data.split("\n");
+
+    let tableRows = [];
+    let tableData = [];
+
+    let isTr = false;
+
+    // gets table rows from html
+    for(let i = 0; i < lines.length; i++){
+        if(lines[i].includes("<tr>")){
+            isTr = true;
+        } else if(lines[i].includes("</tr>")){
+            isTr = false;
+        } else if(isTr){
+            tableRows.push(lines[i]);
+        }
+    }
+
+    // var killersInfo = {};
+    // var killerNames = [];
+    let cols = [];
+
+    let colCount = 0;
+
+    count = 0;
+
+    // gets data (td) from table rows (tr)
+    // for each serial killer
+    // 6 cols per row
+    for(let i = 0; i < tableRows.length; i++){
+        // if(lines[i] == undefined) continue;
+
+        if(colCount == 6){
+            // COLUMN: name
+            let nameTD = cols[0]
+
+            // get name from within anchor tag in format <td><a...></a></td>.
+            // find closing anchor tag
+            let anchorClosingInd = nameTD.indexOf("</a>");
+
+            // work backwards from closing anchor closing arrow of opening anchor tag, e.g. the > of <a..>
+            let name = [];
+
+            for(let i = anchorClosingInd - 1; nameTD[i] != ">"; i--){
+                name.push(nameTD[i]);
+            }
+
+            // reverse name and convert to string
+            name = name.reverse().join("");
+
+            killerNames.push(name);
+            killersInfo[name] = {};
+
+            // COLUMN: country
+            let countryTD = cols[1].split(">");
+            let countries = [];
+
+            // get countries from title attributes
+            inTitle = 0;
+
+            for(let i = 0; i < countryTD.length; i++){
+                let item = countryTD[i];
+
+                if(item.includes("title=")){
+                    let tmp = item.split('"');
+                    let country = tmp[tmp.length - 2].replace("Soviet Union", "Russia");
+                    countries.push(country);
+                }
+            }
+
+            killersInfo[name]["countries"] = countries
+
+
+            // COLUMN: years active
+            let yearsActiveTD = cols[2];
+            let yearsActiveTxt = "";
+
+            // get text from within td tag - i = 4 to skip opening <td>
+            for(let i = 4; yearsActiveTD[i] != "<"; i++){
+                yearsActiveTxt += yearsActiveTD[i];
+            }
+
+            let tmp = yearsActiveTxt.replace("?", "").split(" to ");
+            let yearsActive = {"start": parseInt(tmp[0]), "end": parseInt(tmp[1])};
+
+            killersInfo[name]["Years Active"] = yearsActive;
+
+            // COLUMN: proven victims
+            let provenVictsTD = cols[3];
+            let provenVicts = provenVictsTD.substring(4, provenVictsTD.length - 5);
+
+            killersInfo[name]["Proven Victims"] = parseInt(provenVicts);
+
+            // COLUMN: possible victims
+            let possibleVictsTD = cols[4];
+
+            // COLUMN: notes
+            let notesTD = cols[5].substring(4, cols[5].length);
+
+            killersInfo[name]["notes"] = notesTD;
+
+            colCount = 0;
+            cols = [];
+        }
+
+        if(lines[i].includes("<td>")){
+            cols.push(lines[i])
+            colCount++;
+            count++;
+        }
+    }
+}
